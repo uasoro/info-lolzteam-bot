a6adf01c0e512f853a4d0293e58fdf786b645314
commandlzt <155655800+commandlzt@users.noreply.github.com>
2024-01-04 22:58:42 +0700

    Add files via upload

---
+++ lztinfobot/config.py
@@ -0,0 +1,15 @@
+#данные
+lzt_token = 'Bearer ' + 'ваш токен лолза'
+tg_token = 'ваш токен телеграм'
+id_user_lzt = 'ваш айди лолза'
+
+#реквизиты, кошельки
+BTC = 'bc1qnxctf6l97rfxzmxv6h42qkpqg26g4nc5r09x44'
+usdt = 'TKC73SBEo8FUBJSvgpGb7XF6ww7aK5uXjJ'
+card = 'ваша карта'
+payment = f'карта - `{card}`\nBTC - `{BTC}`\nusdt(tron20) - `{usdt}`'
+
+#контакты
+user = "@ваш_юзернейм"
+github = 'ссылка на профиль гитхаб'
+contacts = f'Telegram - {user}\ngithub - {github}'
---
+++ lztinfobot/lztinfobot.py
@@ -0,0 +1,39 @@
+import telebot
+import requests
+import json
+from config import lzt_token, id_user_lzt, tg_token, payment, contacts, user
+
+bot = telebot.TeleBot(tg_token)
+
+#запрос и получение данных
+request = requests.get('https://api.zelenka.guru/users?page='+id_user_lzt+'&limit=1',headers={'Authorization':lzt_token})
+data = json.loads(request.text)
+username = (data['users'][0]['username'])
+user_like_count = (data['users'][0]['user_like_count'])
+user_title = (data['users'][0]['user_title'])
+permalink = (data['users'][0]['links']['permalink'])
+photo = (data['users'][0]['links']['avatar_big'])
+
+#сбор инфы
+info = (f'| ZELENKA.GURU |\n\nАватар - {photo}\n'
+	 f'Профиль - `{permalink}`\n'
+	 f'Ник - `{username}`\n'
+	 f'Количество симпатий: `{user_like_count}`\n'
+	 f'Статус: `{user_title}`\n\n| {user} |')
+
+contacts = f'lolz - {permalink}\n' + contacts
+
+#отправка в тг
+@bot.message_handler(commands=['start'])
+def send_lessons(message):
+	bot.reply_to(message, info, parse_mode="MARKDOWN")
+
+@bot.message_handler(commands=['payments'])
+def send_lessons(message):
+	bot.reply_to(message, payment, parse_mode="MARKDOWN")
+
+@bot.message_handler(commands=['contacts'])
+def send_lessons(message):
+	bot.reply_to(message, contacts)
+	
+bot.polling()
